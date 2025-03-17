795c44034885645675ec90c6b44d2c024b5e1343
MOHAMMED OWAIS <88034859+Ubbix1@users.noreply.github.com>
2025-03-18 00:19:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import os
+from flask import Flask, request
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from flask_limiter import Limiter
@@ -12,10 +13,12 @@ CORS(app)
 JWTManager(app)
 
 # Rate limiting (prevent brute force attacks)
-limiter = Limiter(app, key_func=lambda: request.remote_addr)
+limiter = Limiter(key_func=lambda: request.remote_addr)
+limiter.init_app(app)
 
 app.register_blueprint(user_bp, url_prefix="/api/users")
 app.register_blueprint(prediction_bp, url_prefix="/api/predict")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Ensure correct port for Render
+    app.run(host="0.0.0.0", port=port, debug=True)
