6a101117310c9138b28e19209aa33e2a9c4f5b3b
MOHAMMED OWAIS <88034859+Ubbix1@users.noreply.github.com>
2025-03-18 22:05:34 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-# Load database URL from environment variables or use a default
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ubbixcode:X3T6Yix71ul9kdmd@cluster0.vntmb.mongodb.net/")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "sign_language_db")  # Set default DB name
-
-# Initialize MongoDB client
-client = AsyncIOMotorClient(MONGO_URI)
-db = client[DATABASE_NAME]  # Now explicitly specifying a database
-
 class Database:
-    """MongoDB database connection"""
-    @staticmethod
-    def get_db():
-        return db
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")  # Use Render's env variable
+    DATABASE_NAME = os.getenv("DATABASE_NAME", "sign_language_db")  # Default DB name
+
+    client = AsyncIOMotorClient(MONGO_URI)
+    db = client[DATABASE_NAME]  # Correctly set the database instance
