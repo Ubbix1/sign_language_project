b792cd365e61d94ca0754fd7bd088df313f4206e
MOHAMMED OWAIS <88034859+Ubbix1@users.noreply.github.com>
2025-03-24 05:33:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()  # Load environment variables from .env file
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "super_secret_key")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ubbixcode:X3T6Yix71ul9kdmd@cluster0.vntmb.mongodb.net/")
+    MONGO_URI = os.getenv("MONGO_URI", "")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt_secret")
     JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hour
     JWT_REFRESH_TOKEN_EXPIRES = 86400  # 1 day
